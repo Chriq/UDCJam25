558960df00d3b79e0127498c4e2309aff08288b5
Christian Cook <christiancook52@gmail.com>
2023-03-03 22:38:06 -0500

    Added simple grid movement

---
+++ UDCJam25/Assets/Scripts/GameManager.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+    public Board board;
+
+    private static GameManager _instance;
+    public static GameManager Instance { 
+        get { 
+            if(_instance == null) {
+                _instance = new GameObject("GameManager").AddComponent<GameManager>();
+            }
+            return _instance;
+        }
+    }
+
+	private void Awake() {
+        board = null;
+        GameObject boardObj = GameObject.Find("Board");
+        
+        if(boardObj != null ) {
+            board = boardObj.GetComponent<Board>();
+		}
+
+        if(board == null) {
+            board = new GameObject("Board").AddComponent<Board>();
+        }
+		DontDestroyOnLoad(gameObject);
+	}
+}
