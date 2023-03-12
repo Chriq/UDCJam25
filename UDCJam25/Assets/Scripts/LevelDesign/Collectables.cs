b1e39ceff0ff64df65f9043971ab177387bf73f8
Jack Chandler <jtc4hnu@virginia.edu>
2023-03-12 16:59:22 -0400

    Unused Collactables + Lore

---
+++ UDCJam25/Assets/Scripts/LevelDesign/Collectables.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+ * 
+ * LORE DUMP
+
+Andrew Carnegie
+
+-Diary of Alice Bank, Steel Enchanter
+"The Champion of the Working Man? I highly doubt that I've heard he's slaughtering pixies in the plants to make the steel better."
+
+-The Reader's Guide to Industrialized Reading, Ch 2
+Libraries. Libraries are the future. The wealth of knowledge preserved by non-fictional writings have dratically improved the quality of life of the orge tribes. From barbanism to intellectualism, the ogres are now perhaps the smartest species known.
+
+-Association of Libaral Plants Poltiical Debate
+We the plants of this nation, in an act of selfless devotion to equality, driven by the decimation of urban gardens for the purpose of surplus agriculture, foreseen by the social elite and inhibited by corporate stability regulations, condemn the power bestowed upon the wealthy.
+
+-History 1
+Andrew Carnegie is an American industrialist. He became a prominant leader of the steel industry after investing in other industries such as iron and oil.
+
+-History 2
+Andrew Carnegie's reputation was smeeared by the Homestead Strikein 1982, in which workers on strike had a fierce battle with armed Pinkerton guards. The strike ended in the union's defeat, and the labor movememnt in the local area was crippled.
+
+-History 3
+Andrew Carnegie considered himself the champion of the working man. In his later years, he devoted himself to philanthropy. Carnegie donated the bulk of his wealth to build public libraries, church organs, and also the concert venue Carngie Hall.
+
+
+
+JP Morgan
+
+-Demon's Delight News
+In yet another economic feint, JPMorgan has pulled off another financial feat by taking control of the national finances and forcing loans from his own busniesses. This outstanding individual is manipulating the government deirectly. That sure is something to be proud of.
+
+-Lore 2
+
+-Lore 3
+
+-History 1
+John Pierpont Morgan was a powerful banker involved in U.S. Steel and General Electric. He formed the predececsor to the modern JPMorgan Chase.
+
+-History 2
+At the time, the US governmen had no central banking authority. Morgan used his influence to stabilize economic crises by loaning the government money and encouraging bail outs for failing finanacial institutions.
+
+-History 3
+Criticism arrose surrounding the control Morgan had gained over the financial system. 
+*/
+
+public class Collectables : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
